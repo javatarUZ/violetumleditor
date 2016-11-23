7fda174239ced4038a1c018e4c4e73324be0d1c6
Sebastian Podgórski <sebastian.podgorski94@gmail.com>
2016-11-23 22:00:12 +0100

    [I] Added methods to enumeration diagram class

--- violetplugin-classdiagram/src/main/java/com/horstmann/violet/product/diagram/classes/node/EnumNode.java
+++ violetplugin-classdiagram/src/main/java/com/horstmann/violet/product/diagram/classes/node/EnumNode.java
@@ -13,29 +13,37 @@ import com.horstmann.violet.product.diagram.property.text.LineText;
 import com.horstmann.violet.product.diagram.property.text.MultiLineText;
 import com.horstmann.violet.product.diagram.property.text.SingleLineText;
 
+import com.horstmann.violet.product.diagram.property.text.decorator.RemoveSentenceDecorator;
+import com.horstmann.violet.product.diagram.property.text.decorator.ReplaceSentenceDecorator;
+import com.horstmann.violet.product.diagram.property.text.decorator.UnderlineDecorator;
 import java.awt.*;
 
 /**
- * A class node in a class diagram.
+ * Enumeration diagram class
  */
 public class EnumNode extends ColorableNode
 {
-	/**
-     * Construct a class node with a default size
-     */
+
     public EnumNode()
     {
         super();
         name = new SingleLineText(NAME_CONVERTER);
-        attributes = new MultiLineText();
+        attributes = new MultiLineText(PROPERTY_CONVERTER);
+        methods = new MultiLineText(PROPERTY_CONVERTER);
         createContentStructure();
     }
 
-    protected EnumNode(EnumNode node) throws CloneNotSupportedException
+    /**
+     * Constructor responsible for cloning enumeration diagram
+     * @param node
+     * @throws CloneNotSupportedException
+     */
+    protected EnumNode(final EnumNode node) throws CloneNotSupportedException
     {
         super(node);
         name = node.name.clone();
         attributes = node.attributes.clone();
+        methods = node.methods.clone();
         createContentStructure();
     }
 
@@ -52,8 +60,14 @@ public class EnumNode extends ColorableNode
         {
             attributes = new MultiLineText();
         }
+        if(null == methods)
+        {
+            methods = new MultiLineText();
+        }
         name.reconstruction(NAME_CONVERTER);
-        attributes.reconstruction();
+        attributes.reconstruction(PROPERTY_CONVERTER);
+        methods.reconstruction(PROPERTY_CONVERTER);
+        name.setAlignment(LineText.CENTER);
     }
 
     @Override
@@ -71,10 +85,12 @@ public class EnumNode extends ColorableNode
         nameContent.setMinHeight(MIN_NAME_HEIGHT);
         nameContent.setMinWidth(MIN_WIDTH);
         TextContent attributesContent = new TextContent(attributes);
+        TextContent methodsContent = new TextContent(methods);
 
         VerticalLayout verticalGroupContent = new VerticalLayout();
         verticalGroupContent.add(nameContent);
         verticalGroupContent.add(attributesContent);
+        verticalGroupContent.add(methodsContent);
         separator = new Separator.LineSeparator(getBorderColor());
         verticalGroupContent.setSeparator(separator);
 
@@ -88,7 +104,7 @@ public class EnumNode extends ColorableNode
     }
 
     @Override
-    public void setBorderColor(Color borderColor)
+    public void setBorderColor(final Color borderColor)
     {
         if(null != separator)
         {
@@ -98,7 +114,7 @@ public class EnumNode extends ColorableNode
     }
 
     @Override
-    public void setTextColor(Color textColor)
+    public void setTextColor(final Color textColor)
     {
         name.setTextColor(textColor);
         attributes.setTextColor(textColor);
@@ -111,53 +127,52 @@ public class EnumNode extends ColorableNode
         return ClassDiagramConstant.CLASS_DIAGRAM_RESOURCE.getString("tooltip.enum_node");
     }
 
-    /**
-     * Sets the name property value.
-     * 
-     * @param newValue the class name
-     */
-    public void setName(LineText newValue)
+    public void setName(final LineText newValue)
     {
         name.setText(newValue);
     }
 
-    /**
-     * Gets the name property value.
-     * 
-     * @return the class name
-     */
     public LineText getName()
     {
         return name;
     }
 
-    /**
-     * Sets the attributes property value.
-     *
-     * @param newValue the attributes of this class
-     */
-    public void setAttributes(LineText newValue)
+    public void setAttributes(final LineText newValue)
     {
         attributes.setText(newValue);
     }
 
-    /**
-     * Gets the attributes property value.
-     *
-     * @return the attributes of this class
-     */
     public MultiLineText getAttributes()
     {
         return attributes;
     }
 
+    public void setMethods(final LineText newValue)
+    {
+        methods.setText(newValue);
+    }
+
+    public LineText getMethods()
+    {
+        return methods;
+    }
+
     private SingleLineText name;
     private MultiLineText attributes;
+    private MultiLineText methods;
 
     private transient Separator separator;
 
     private static final int MIN_NAME_HEIGHT = 45;
     private static final int MIN_WIDTH = 100;
+
+    private static final String[][] SIGNATURE_REPLACE_KEYS = {
+        { "public ", "+ " },
+        { "package ", "~ " },
+        { "protected ", "# " },
+        { "private ", "- " },
+        { "property ", "/ " }
+    };
 
     private static final LineText.Converter NAME_CONVERTER = new LineText.Converter()
     {
@@ -165,6 +180,22 @@ public class EnumNode extends ColorableNode
         public OneLineText toLineString(String text)
         {
             return new PrefixDecorator( new LargeSizeDecorator(new OneLineText(text)), "<center>«enumeration»</center>");
+        }
+    };
+
+    private static final LineText.Converter PROPERTY_CONVERTER = new LineText.Converter()
+    {
+        @Override
+        public OneLineText toLineString(String text)
+        {
+            OneLineText lineString = new OneLineText(text);
+
+            for(String[] signature : SIGNATURE_REPLACE_KEYS)
+            {
+                lineString = new ReplaceSentenceDecorator(lineString, signature[0], signature[1]);
+            }
+
+            return lineString;
         }
     };
 }
