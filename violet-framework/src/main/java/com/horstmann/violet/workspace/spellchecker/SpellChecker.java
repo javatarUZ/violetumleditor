b9856fab94f6c99b775de4b5afd0f5e20e51a47d
Sebastian Podgórski <sebastian.podgorski94@gmail.com>
2017-01-07 23:13:24 +0100

    [I] Added SpellChecker class

---
+++ violet-framework/src/main/java/com/horstmann/violet/workspace/spellchecker/SpellChecker.java
@@ -0,0 +1,34 @@
+package com.horstmann.violet.workspace.spellchecker;
+
+import java.io.IOException;
+import java.util.List;
+import org.languagetool.JLanguageTool;
+import org.languagetool.language.BritishEnglish;
+import org.languagetool.rules.Rule;
+import org.languagetool.rules.RuleMatch;
+
+/**
+ * This class check correctness of string
+ *
+ * @author Sebastian Podgorski
+ */
+public class SpellChecker {
+
+    private SpellChecker(){}
+
+    public static boolean isCorrectWord(final String word) throws IOException {
+        JLanguageTool langTool = new JLanguageTool(new BritishEnglish());
+        for (Rule rule : langTool.getAllRules()) {
+            if (!rule.isDictionaryBasedSpellingRule()) {
+                langTool.disableRule(rule.getId());
+            }
+        }
+        List<RuleMatch> matches = langTool.check(word);
+        int numberOfErrors = matches.size();
+        if (numberOfErrors == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
