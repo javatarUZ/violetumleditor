631fe7743e8ad5a6dc47fd8c8c365dc2f4fd0c22
Tomasz Cichocki <11frez11@gmail.com>
2017-01-22 16:19:07 +0100

    [I] Changes based on SonarLint tips

--- violet-framework/src/main/java/com/horstmann/violet/workspace/editorpart/EditorPart.java
+++ violet-framework/src/main/java/com/horstmann/violet/workspace/editorpart/EditorPart.java
@@ -43,251 +43,8 @@ import javax.swing.JComponent;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-/**
- * Graph editor
- */
 public class EditorPart extends JPanel implements IEditorPart
 {
-
-    /**
-     * Default constructor
-     *
-     * @param aGraph graph which will be drawn in this editor part
-     */
-    public EditorPart(IGraph aGraph)
-    {
-        ResourceBundleInjector.getInjector().inject(this);
-        this.graph = aGraph;
-        this.zoom = 1;
-        this.grid = new PlainGrid(this);
-        this.graph.setGridSticker(grid.getGridSticker());
-        addMouseListener(new MouseAdapter()
-        {
-
-            public void mousePressed(MouseEvent event)
-            {
-                behaviorManager.fireOnMousePressed(event);
-            }
-
-            public void mouseReleased(MouseEvent event)
-            {
-                behaviorManager.fireOnMouseReleased(event);
-            }
-
-            public void mouseClicked(MouseEvent event)
-            {
-                behaviorManager.fireOnMouseClicked(event);
-            }
-        });
-
-        addMouseWheelListener(new MouseWheelListener()
-        {
-            @Override
-            public void mouseWheelMoved(MouseWheelEvent e)
-            {
-                behaviorManager.fireOnMouseWheelMoved(e);
-            }
-        });
-
-        addMouseMotionListener(new MouseMotionAdapter()
-        {
-            public void mouseDragged(MouseEvent event)
-            {
-                behaviorManager.fireOnMouseDragged(event);
-            }
-
-            @Override
-            public void mouseMoved(MouseEvent event)
-            {
-                behaviorManager.fireOnMouseMoved(event);
-            }
-        });
-        setBounds(0, 0, 0, 0);
-        setDoubleBuffered(false);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.horstmann.violet.framework.display.clipboard.IEditorPart#getGraph()
-     */
-    public IGraph getGraph()
-    {
-        return this.graph;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.horstmann.violet.framework.display.clipboard.IEditorPart#removeSelected()
-     */
-    public void removeSelected()
-    {
-        if (!isUserSureToRemoveSelectedObjects()) return;
-        this.behaviorManager.fireBeforeRemovingSelectedElements();
-        try
-        {
-            List<INode> selectedNodes = selectionHandler.getSelectedNodes();
-            List<IEdge> selectedEdges = selectionHandler.getSelectedEdges();
-            IEdge[] edgesArray = selectedEdges.toArray(new IEdge[selectedEdges.size()]);
-            INode[] nodesArray = selectedNodes.toArray(new INode[selectedNodes.size()]);
-            graph.removeNode(nodesArray);
-            graph.removeEdge(edgesArray);
-        }
-        finally
-        {
-            this.selectionHandler.clearSelection();
-            this.behaviorManager.fireAfterRemovingSelectedElements();
-        }
-    }
-
-    private boolean isUserSureToRemoveSelectedObjects()
-    {
-        int reply = JOptionPane.showConfirmDialog(null, deleteDialogMessage, deleteDialogTitle, JOptionPane.YES_NO_OPTION);
-        if (reply == JOptionPane.YES_OPTION)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    public List<INode> getSelectedNodes()
-    {
-        return selectionHandler.getSelectedNodes();
-    }
-
-    public void clearSelection()
-    {
-        selectionHandler.clearSelection();
-    }
-
-    public void selectElement(INode node)
-    {
-        selectionHandler.addSelectedElement(node);
-    }
-
-    @Override
-    public Dimension getPreferredSize()
-    {
-        Dimension parentSize = getParent().getSize();
-        Rectangle2D bounds = graph.getClipBounds();
-        int width = Math.max((int) (zoom * bounds.getMaxX()), (int) parentSize.getWidth());
-        int height = Math.max((int) (zoom * bounds.getMaxY()), (int) parentSize.getHeight());
-        if (this.lastWidth != width || this.lastHeight != height)
-        {
-            this.lastWidth = width;
-            this.lastHeight = height;
-        }
-        return new Dimension(width, height);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.horstmann.violet.framework.display.clipboard.IEditorPart#changeZoom(int)
-     */
-    public void changeZoom(int steps)
-    {
-        final double FACTOR = Math.sqrt(Math.sqrt(2));
-        for (int i = 1; i <= steps; i++)
-            zoom *= FACTOR;
-        for (int i = 1; i <= -steps; i++)
-            zoom /= FACTOR;
-        invalidate();
-        repaint();
-    }
-
-    @Override
-    public double getZoomFactor()
-    {
-        return this.zoom;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.horstmann.violet.product.workspace.editorpart.IEditorPart#getGrid()
-     */
-    public IGrid getGrid()
-    {
-        return this.grid;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @seecom.horstmann.violet.framework.workspace.editorpart.IEditorPart# growDrawingArea()
-     */
-    public void growDrawingArea()
-    {
-        IGraph g = getGraph();
-        Rectangle2D bounds = g.getClipBounds();
-        bounds.add(getBounds());
-        g.setBounds(new Double(0, 0, GROW_SCALE_FACTOR * bounds.getWidth(), GROW_SCALE_FACTOR * bounds.getHeight()));
-        invalidate();
-        repaint();
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @seecom.horstmann.violet.framework.workspace.editorpart.IEditorPart# clipDrawingArea()
-     */
-    public void clipDrawingArea()
-    {
-        IGraph g = getGraph();
-        g.setBounds(null);
-        invalidate();
-        repaint();
-    }
-
-    public JComponent getSwingComponent()
-    {
-        return this;
-    }
-
-    @Override
-    public void paintImmediately(int x, int y, int w, int h)
-    {
-        getSwingComponent().invalidate();
-        super.paintImmediately(x, y, w, h);
-    }
-
-    @Override
-    protected void paintComponent(Graphics g)
-    {
-        boolean valid = getSwingComponent().isValid();
-        if (valid)
-        {
-            return;
-        }
-        getSwingComponent().revalidate(); // to inform parent scrollpane container
-        Graphics2D g2 = (Graphics2D) g;
-        g2.scale(zoom, zoom);
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
-        if (grid.isVisible()) grid.paint(g2);
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        graph.draw(g2);
-        for (IEditorPartBehavior behavior : this.behaviorManager.getBehaviors())
-        {
-            behavior.onPaint(g2);
-        }
-    }
-
-    @Override
-    public IEditorPartSelectionHandler getSelectionHandler()
-    {
-        return this.selectionHandler;
-    }
-
-    @Override
-    public IEditorPartBehaviorManager getBehaviorManager()
-    {
-        return this.behaviorManager;
-    }
 
     private IGraph graph;
 
@@ -313,5 +70,227 @@ public class EditorPart extends JPanel implements IEditorPart
     private static final double GROW_SCALE_FACTOR = Math.sqrt(2);
 
     private IEditorPartBehaviorManager behaviorManager = new EditorPartBehaviorManager();
+
+    public EditorPart(IGraph aGraph)
+    {
+        ResourceBundleInjector.getInjector().inject(this);
+        this.graph = aGraph;
+        this.zoom = 1;
+        this.grid = new PlainGrid(this);
+        this.graph.setGridSticker(grid.getGridSticker());
+        addMouseListener(new MouseAdapter()
+        {
+            @Override
+            public void mousePressed(MouseEvent event)
+            {
+                behaviorManager.fireOnMousePressed(event);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent event)
+            {
+                behaviorManager.fireOnMouseReleased(event);
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent event)
+            {
+                behaviorManager.fireOnMouseClicked(event);
+            }
+        });
+
+        addMouseWheelListener(new MouseWheelListener()
+        {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e)
+            {
+                behaviorManager.fireOnMouseWheelMoved(e);
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionAdapter()
+        {
+            @Override
+            public void mouseDragged(MouseEvent event)
+            {
+                behaviorManager.fireOnMouseDragged(event);
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent event)
+            {
+                behaviorManager.fireOnMouseMoved(event);
+            }
+        });
+        setBounds(0, 0, 0, 0);
+        setDoubleBuffered(false);
+    }
+
+    @Override
+    public IGraph getGraph()
+    {
+        return this.graph;
+    }
+
+    @Override
+    public void removeSelected()
+    {
+        if (isUserSureToRemoveSelectedObjects())
+        {
+            this.behaviorManager.fireBeforeRemovingSelectedElements();
+            try
+            {
+                List<INode> selectedNodes = selectionHandler.getSelectedNodes();
+                List<IEdge> selectedEdges = selectionHandler.getSelectedEdges();
+                IEdge[] edgesArray = selectedEdges.toArray(new IEdge[selectedEdges.size()]);
+                INode[] nodesArray = selectedNodes.toArray(new INode[selectedNodes.size()]);
+                graph.removeNode(nodesArray);
+                graph.removeEdge(edgesArray);
+            }
+            finally
+            {
+                this.selectionHandler.clearSelection();
+                this.behaviorManager.fireAfterRemovingSelectedElements();
+            }
+        }
+    }
+
+    private boolean isUserSureToRemoveSelectedObjects()
+    {
+        int reply = JOptionPane.showConfirmDialog(null, deleteDialogMessage, deleteDialogTitle, JOptionPane.YES_NO_OPTION);
+        return reply == JOptionPane.YES_OPTION;
+    }
+
+    @Override
+    public List<INode> getSelectedNodes()
+    {
+        return selectionHandler.getSelectedNodes();
+    }
+
+    @Override
+    public void clearSelection()
+    {
+        selectionHandler.clearSelection();
+    }
+
+    @Override
+    public void selectElement(INode node)
+    {
+        selectionHandler.addSelectedElement(node);
+    }
+
+    @Override
+    public Dimension getPreferredSize()
+    {
+        Dimension parentSize = getParent().getSize();
+        Rectangle2D bounds = graph.getClipBounds();
+        int width = Math.max((int) (zoom * bounds.getMaxX()), (int) parentSize.getWidth());
+        int height = Math.max((int) (zoom * bounds.getMaxY()), (int) parentSize.getHeight());
+        if (this.lastWidth != width || this.lastHeight != height)
+        {
+            this.lastWidth = width;
+            this.lastHeight = height;
+        }
+        return new Dimension(width, height);
+    }
+
+    /**
+     * Changes the zoom of this editor. The zoom is 1 by default and is multiplied by sqrt(2) for each positive stem or divided by
+     * sqrt(2) for each negative step.
+     *
+     * @param steps the number of steps by which to change the zoom. A positive value zooms in, a negative value zooms out.
+     */
+    @Override
+    public void changeZoom(int steps)
+    {
+        final double factor = Math.sqrt(Math.sqrt(2));
+        for (int i = 1; i <= steps; i++)
+            zoom *= factor;
+        for (int i = 1; i <= -steps; i++)
+            zoom /= factor;
+        invalidate();
+        repaint();
+    }
+
+    @Override
+    public double getZoomFactor()
+    {
+        return this.zoom;
+    }
+
+    @Override
+    public IGrid getGrid()
+    {
+        return this.grid;
+    }
+
+    @Override
+    public void growDrawingArea()
+    {
+        IGraph g = getGraph();
+        Rectangle2D bounds = g.getClipBounds();
+        bounds.add(getBounds());
+        g.setBounds(new Double(0, 0, GROW_SCALE_FACTOR * bounds.getWidth(), GROW_SCALE_FACTOR * bounds.getHeight()));
+        invalidate();
+        repaint();
+    }
+
+    @Override
+    public void clipDrawingArea()
+    {
+        IGraph g = getGraph();
+        g.setBounds(null);
+        invalidate();
+        repaint();
+    }
+
+    @Override
+    public JComponent getSwingComponent()
+    {
+        return this;
+    }
+
+    @Override
+    public void paintImmediately(int x, int y, int w, int h)
+    {
+        getSwingComponent().invalidate();
+        super.paintImmediately(x, y, w, h);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        boolean valid = getSwingComponent().isValid();
+        if (valid)
+        {
+            return;
+        }
+        getSwingComponent().revalidate(); // to inform parent scrollpane container
+        Graphics2D g2 = (Graphics2D) g;
+        g2.scale(zoom, zoom);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+        if (grid.isVisible())
+        {
+            grid.paint(g2);
+        }
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        graph.draw(g2);
+        for (IEditorPartBehavior behavior : this.behaviorManager.getBehaviors())
+        {
+            behavior.onPaint(g2);
+        }
+    }
+
+    @Override
+    public IEditorPartSelectionHandler getSelectionHandler()
+    {
+        return this.selectionHandler;
+    }
+
+    @Override
+    public IEditorPartBehaviorManager getBehaviorManager()
+    {
+        return this.behaviorManager;
+    }
 
 }
