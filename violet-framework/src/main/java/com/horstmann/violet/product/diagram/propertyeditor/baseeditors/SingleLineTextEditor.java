5fb0bb6309daf603464c2af461c7838b0300fd48
Krzysztof Perchlicki <krzysztof.perchlicki@gmail.com>
2017-01-25 16:06:08 +0100

    small fixes

--- violet-framework/src/main/java/com/horstmann/violet/product/diagram/propertyeditor/baseeditors/SingleLineTextEditor.java
+++ violet-framework/src/main/java/com/horstmann/violet/product/diagram/propertyeditor/baseeditors/SingleLineTextEditor.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 import javax.swing.text.JTextComponent;
 
 /**
- * A property editor for the MultiLineText type.
+ * A property editor for the SingleLineText type.
  */
 public class SingleLineTextEditor extends LineTextEditor
 {
